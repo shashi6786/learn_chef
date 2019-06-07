09126df71584e6090dce9313968b36b2ba12de2a
Ubuntu <ubuntu@ip-172-31-32-75.ec2.internal>
2019-06-07 03:26:57 +0000

    Moving index.html content to a template file and reffering the template in recipe

--- manage_apache_cookbook/recipes/default.rb
+++ manage_apache_cookbook/recipes/default.rb
@@ -1,5 +1,23 @@
-#
-# Cookbook:: manage_apache_cookbook
-# Recipe:: default
-#
-# Copyright:: 2019, The Authors, All Rights Reserved.
+apt_update 'Update the apt cache daily' do
+	frequency 86_400
+	action :periodic
+end
+
+package 'apache2'
+
+service 'apache2' do
+	action [:enable, :start]
+end
+
+#file '/var/www/html/index.html' do
+#	content '<html>
+#	<body>
+#		<h1> Learning Chef</h1>
+#	</body>
+#	</html>'
+#end
+
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
+end
+
